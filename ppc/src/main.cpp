2f7108f07d7ea04534276d4f61e716109cd1c926
Simon <homer12345@gmail.com>
2024-06-22 18:20:46 -0700

    Fix log init

--- ppc/src/main.cpp
+++ ppc/src/main.cpp
@@ -28,29 +28,7 @@ extern "C" DLLEXPORT constinit auto F4SEPlugin_Version = []() noexcept {
 
 extern "C" DLLEXPORT bool F4SEAPI F4SEPlugin_Load(const F4SE::LoadInterface* a_f4se)
 {
-    auto path = logger::log_directory();
-    if (!path)
-    {
-        return false;
-    }
-
-    *path /= fmt::format(FMT_STRING("{}.log"), Version::PROJECT);
-    auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true);
-
-    auto log  = std::make_shared<spdlog::logger>("global log"s, std::move(sink));
-
-#ifndef NDEBUG
-    log->set_level(spdlog::level::trace);
-    spdlog::flush_on(spdlog::level::debug);
-#else
-    log->set_level(spdlog::level::info);
-    log->flush_on(spdlog::level::warn);
-#endif
-
-    spdlog::set_default_logger(std::move(log));
-    spdlog::set_pattern("%g(%#): [%^%l%$] %v"s);
-
-    logger::info("{} v{}", Version::PROJECT, Version::NAME);
+    F4SE::Init(a_f4se, true);
 
     if (a_f4se->IsEditor())
     {
@@ -64,8 +42,6 @@ extern "C" DLLEXPORT bool F4SEAPI F4SEPlugin_Load(const F4SE::LoadInterface* a_f
         logger::critical("unsupported runtime v{}", ver.string());
         return false;
     }
-
-    F4SE::Init(a_f4se);
 
     // Load config
     PPC::Config::GetSingleton()->LoadConfig();
